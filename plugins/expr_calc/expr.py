c0f48a79d9fb36b23f61ff31893a6c97c0bea6b9
bd <Kudu0001>
2018-08-27 23:04:27 -0700

    workit works now

--- plugins/expr_calc/expr.py
+++ plugins/expr_calc/expr.py
@@ -49,17 +49,7 @@ def poly(plugin_name, coeff_arr):
 
     def fn_generate_data(num_samples):
         nonlocal  abs_path_to_json_scratch_file
-        inputs = []
-        outputs = []
-
-        for _ in range(num_samples):
-            inp_array, out_int = fn_generate_one_sample(NUM_OF_INPUT_PARTS)
-            inp_str = ''
-            for inp_item in inp_array:
-                s = str(inp_item)
-                inp_str = inp_str + s
-            inputs.append(inp_str)
-            outputs.append(str(round(out_int)))
+        inputs, outputs = generate_data_from_random_numbers(num_samples)
 
         maxlen_inputs = get_maxlen_of_listitems(inputs)
         maxlen_labels = get_maxlen_of_listitems(outputs)
@@ -70,26 +60,45 @@ def poly(plugin_name, coeff_arr):
 
         return (inputs, labels)
 
-    def input_from_random_number_generator(num_of_samples):
-        i = 0
-        while i < num_of_samples:
-            inp_number = np.random.randint(10)
-            inp_str = str(inp_number)
-            out_str = inp_str_to_out_str(inp_str)
-            yield inp_str, out_str
-            i += 1
+    def generate_data_from_random_numbers(num_samples):
+
+        fn_iterate = random_number_generator_mgr(num_samples)
+        inp_str_arr, out_str_arr = create_set_of_inpstr_outstr_pairs(fn_iterate)
+        return inp_str_arr, out_str_arr
+
+    def random_number_generator_mgr(num_of_samples):
+        def random_number_generator():
+            count = 0
+            while count < num_of_samples:
+                inp_arr = []
+                for i in range(NUM_OF_INPUT_PARTS):
+                    inp_digit = np.random.randint(10)
+                    inp_arr.append(inp_digit)
+
+                out_num = convert_inp_str_to_out_str(inp_arr)
+                out_str = str(out_num)
+                inp_str = ''
+                for digit in inp_arr:
+                    d_str = str(digit)
+                    inp_str = inp_str + d_str
+
+                yield inp_str, out_str
+
+                count = count + 1
+
+        return random_number_generator
 
     def workit_file_generator_mgr(inp_strings):
         def workit_file_generator():
             i = 0
             while i < len(inp_strings):
                 inp_str = inp_strings[i]
-                out_str = inp_str_to_out_str(inp_str)
+                out_str = convert_inp_str_to_out_str(inp_str)
                 yield inp_str, out_str
                 i += 1
         return workit_file_generator
 
-    def inp_str_to_out_str(inp_str):
+    def convert_inp_str_to_out_str(inp_str):
         out_num = 0
         for chr in inp_str:
             digit = int(chr)
