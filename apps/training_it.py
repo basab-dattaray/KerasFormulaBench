696bc27ccd1bf4d7bfb2afae9d2ade6880b166e5
bd <Kudu0001>
2018-09-05 21:24:41 -0700

    refactoring training 5

--- apps/training_it.py
+++ apps/training_it.py
@@ -1,5 +1,7 @@
 from __future__ import print_function
 
+from common.model.training_mgt import *
+
 from apps.mediation_mgt import *
 from apps.RUN_CONSTANTS import *
 
@@ -12,11 +14,17 @@ def goodbye():
 plugin_name, fn_get_data, fn_generate_data_given_input_strings,fn_setup_model, fn_train_model, fn_stop_training, fn_compile_model, fn_predict \
     = mediation_mgr()
 
+fn_train_model = training_mgr(plugin_name, NUM_OF_ITERATIONS)
+
 inputs, labels = fn_get_data(num_samples=TRAINING_SIZE)
 
 iteration_num = 1
 while iteration_num  < NUM_OF_ITERATIONS:
     model = fn_setup_model(inputs, labels)
+    # iteration_num = fn_train_model(model, inputs, labels, NUM_OF_ITERATIONS, iteration_num, BATCH_SIZE,  NUM_OF_EPOCHS)
+
+    # def fn_train_model(model, inputs, labels, total_num_of_iterations, current_iteration, batch_size, num_of_epochs):
+
     iteration_num = fn_train_model(model, inputs, labels, NUM_OF_ITERATIONS, iteration_num, BATCH_SIZE,  NUM_OF_EPOCHS)
     print('Interations: {}/{} completed'.format(iteration_num, NUM_OF_ITERATIONS ))
     print('===============================================================================')
--- common/model/training_mgt.py
+++ common/model/training_mgt.py
@@ -2,16 +2,11 @@ from common.misc.fileops import *
 from common.model.training_effort_management import *
 from common.generator.data_bender import *
 
-def training_mgr(fn_compile_model, plugin_name, inputs, labels, save_after_n_iterations):
+def training_mgr(plugin_name, save_after_n_iterations):
 
-    _model = None
     _stop_running = None
-    _x_train = None
-    _y_train = None
-    _x_val = None
-    _y_val = None
 
-    _x_train, _x_val, _y_train, _y_val = data_breaker(inputs, labels)
+
     _abs_model_path = get_abs_path('plugins/' + plugin_name + '/model_data/model')
     _stop_running = False
 
@@ -21,25 +16,21 @@ def training_mgr(fn_compile_model, plugin_name, inputs, labels, save_after_n_ite
         _stop_running = True
 
 
-    def fn_train_model(total_num_of_iterations, current_iteration, batch_size, num_of_epochs):
-        # nonlocal _batch_size, _num_of_epochs
-        # nonlocal _model, _abs_model_path
+    def fn_train_model(model, inputs, labels, total_num_of_iterations, current_iteration, batch_size, num_of_epochs):
         nonlocal _stop_running
-        nonlocal  _x_train, _y_train,  _x_val, _y_val
 
-        # _batch_size = batch_size
-        # _num_of_epochs = num_of_epochs
+        early_stopping_call_back = TrainingContinuationCallback(_fn_stop_training, model, plugin_name)
 
-        early_stopping_call_back = TrainingContinuationCallback(_fn_stop_training, _model, plugin_name)
-
+        _x_train, _x_val, _y_train, _y_val = data_breaker(inputs, labels)
         # print("RANGE:", range(current_iteration, total_num_of_iterations + 1))
 
-        return train_on(_stop_running, _x_train, _x_val, _y_train, _y_val, batch_size, current_iteration, early_stopping_call_back,
+        return train_on(model, _stop_running, _x_train, _x_val, _y_train, _y_val, batch_size, current_iteration, early_stopping_call_back,
                         num_of_epochs, total_num_of_iterations)
 
-    def train_on(_stop_running, _x_train, _x_val, _y_train, _y_val, batch_size, current_iteration, early_stopping_call_back,
+    def train_on(model, _stop_running, _x_train, _x_val, _y_train, _y_val, batch_size, current_iteration, early_stopping_call_back,
                  num_of_epochs, total_num_of_iterations):
         # nonlocal _stop_running
+        iteration = None
         for iteration in range(current_iteration, total_num_of_iterations + 1):
 
             if (_stop_running):
@@ -49,16 +40,16 @@ def training_mgr(fn_compile_model, plugin_name, inputs, labels, save_after_n_ite
             print()
             print('-' * 50)
             print('Iteration', iteration)
-            _model.fit(_x_train, _y_train,
-                       batch_size=batch_size,
-                       epochs=num_of_epochs,
-                       validation_data=(_x_val, _y_val),
-                       callbacks=[early_stopping_call_back],
-                       verbose=2)
+            model.fit(_x_train, _y_train,
+                      batch_size=batch_size,
+                      epochs=num_of_epochs,
+                      validation_data=(_x_val, _y_val),
+                      callbacks=[early_stopping_call_back],
+                      verbose=2)
             if iteration > 0 and iteration % save_after_n_iterations == 0:
-                save_model(_abs_model_path, _model)
+                save_model(_abs_model_path, model)
                 print('saved model after {} iterations'.format(iteration))
-        save_model(_abs_model_path, _model)
+        save_model(_abs_model_path, model)
         print('saved model, normal termination')
         return iteration
     return fn_train_model
