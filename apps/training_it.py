74b4a63bdc51ea9174f59368111f9e64bd1fddf4
bd <Kudu0001>
2018-09-07 23:12:16 -0700

    fixed Noise Management + added iteration

--- apps/training_it.py
+++ apps/training_it.py
@@ -18,18 +18,23 @@ plugin_name, fn_get_data, fn_generate_data_given_input_strings,fn_setup_model, f
 
 fn_train_model = training_mgr(plugin_name, NUM_OF_ITERATIONS)
 
-inputs, labels = fn_get_data(num_samples=TRAINING_SIZE)
 
-fn_add_noise_to_array_of_char_strings = noise_mgr(NOISE_FACTOR)
+def create_noisy_data():
+    inputs, labels = fn_get_data(num_samples=TRAINING_SIZE)
+    fn_add_noise_to_array_of_char_strings = noise_mgr(NOISE_FACTOR)
+    fn_add_noise_to_array_of_char_strings(labels)
+    return inputs, labels
 
-fn_add_noise_to_array_of_char_strings(labels)
+inputs, labels = create_noisy_data()
+
 iteration_num = 1
-# while iteration_num  < NUM_OF_ITERATIONS and iteration_num <= MAX_ITERATIONS:
-model = fn_setup_model(inputs, labels)
-iteration_num = fn_train_model(model, inputs, labels, NUM_OF_ITERATIONS, iteration_num, BATCH_SIZE,  NUM_OF_EPOCHS)
-print('Interations: {}/{} completed'.format(iteration_num, NUM_OF_ITERATIONS ))
-print('===============================================================================')
-print()
+while iteration_num  < NUM_OF_ITERATIONS and iteration_num <= MAX_ITERATIONS:
+    model = fn_setup_model(inputs, labels)
+    iteration_num = fn_train_model(model, inputs, labels, NUM_OF_ITERATIONS, iteration_num, BATCH_SIZE,  NUM_OF_EPOCHS)
+    print('Interations: {}/{} completed'.format(iteration_num, NUM_OF_ITERATIONS ))
+    print('===============================================================================')
+    print()
+    inputs, labels = create_noisy_data()
 
 x = 8
 
